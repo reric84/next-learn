0fec30546d2593e3c745459063cae36ff3c10718
Igor Roy <igor.roy@nordstrom.com>
2024-09-02 00:06:56 +0200

    create invoice

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -15,7 +15,7 @@ const links = [
   { name: 'Home', href: '/ui/dashboard', icon: HomeIcon },
   {
     name: 'Invoices',
-    href: '/ui/invoices',
+    href: '/ui/dashboard/invoices',
     icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/ui/customers', icon: UserGroupIcon },
